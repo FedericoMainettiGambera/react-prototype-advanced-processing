fd203df4f45916acf0e2eb53b4b9c6456491e0bd
FedericoMainettiGambera <federicomainettigambera@gmail.com>
2025-01-22 19:50:40 +0100

    fix deploy

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -4,14 +4,14 @@ import { Separator } from "@radix-ui/react-separator";
 import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_layout")({
-  beforeLoad: ({ context }) => {
-    // commented out so you can navigate pages even if login api doesn't work (due to CORS problems)
-    // if (!context.auth.isSignedIn) {
-    //   throw redirect({
-    //     to: "/signin",
-    //   });
-    // }
-  },
+  // commented out so you can navigate pages even if login api doesn't work (due to CORS problems)
+  // beforeLoad: ({ context }) => {
+  //   if (!context.auth.isSignedIn) {
+  //     throw redirect({
+  //       to: "/signin",
+  //     });
+  //   }
+  // },
   component: LayoutComponent,
 });
 
