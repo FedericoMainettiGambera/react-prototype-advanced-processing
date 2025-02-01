904107c2e6939f2cbbe9c7446d3553d0cfc17a49
FedericoMainettiGambera <federicomainettigambera@gmail.com>
2025-02-01 16:06:50 +0100

    rename env variables

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import { env } from "@/env/client";
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: env.VITE_API_URL,
+  baseURL: env.VITE_ADVANCED_PROCESSING_BASE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
--- src/env/client.ts
+++ src/env/client.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   clientPrefix: "VITE_",
   client: {
-    VITE_API_URL: z.string().url(),
+    VITE_ADVANCED_PROCESSING_BASE_API_URL: z.string().url(),
     VITE_BASE_TABLE_API_URL: z.string().url(),
   },
   runtimeEnv: import.meta.env,
